182ac8e4db79cb2a6112011d0291507d647f570b
madope24 <samatope24@gmail.com>
2022-07-27 12:29:31 +0300

    changes

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+   //const MealDetailScreen({Key? key}) : super(key: key);
+   static const routeName = '/meal-detail';
+   @override
+   Widget build(BuildContext context) {
+     return Center(
+       child: Text(
+         'The meal'
+       ),
+     );
+   }
+ }
